2dd221dbdd69aab8dd5331a9565e1b5e456c2d70
Tomi <garciadelriotomas@gmail.com>
2023-09-26 00:20:58 -0300

    feat(client)/first-view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
+import React from "react";
+import logo from "./../../assets/logo.png";
+import hamburger from "./../../assets/icons/menu.svg"
+
 function Navbar() {
-    return (<nav className='bg-black h-[5rem] w-full'>
-      <a className="text-black" href="">Holis</a>
-    </nav>);
+    return (
+    <nav className='bg-white h-[5rem] w-full flex justify-between items-center px-5'>
+      <img src={logo} alt="logo" />
+      <img src={hamburger} alt="menu" />
+    </nav>
+    );
   }
 
 export default Navbar;
